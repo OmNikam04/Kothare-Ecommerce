9aa5ce2053fdaeb5ebbcaf475839a26e1fbce07d
OmNikam04 <omnikam0404@gmail.com>
2024-06-07 03:07:30 +0530

    backend url in frontend updated

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -46,7 +46,7 @@ export const login = (email, password) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.post(
-      `${process.env.BACKEND_URL}/login`,
+      `${process.env.REACT_APP_BACKEND_URL}/login`,
       { email, password },
       config
     );
@@ -64,7 +64,7 @@ export const register = (userData) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "multipart/form-data" } };
 
-    const { data } = await axios.post(`${process.env.BACKEND_URL}/register`, userData, config);
+    const { data } = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/register`, userData, config);
 
     dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
   } catch (error) {
@@ -81,7 +81,7 @@ export const loadUser = () => async (dispatch) => {
   try {
     dispatch({ type: LOAD_USER_REQUEST });
 
-    const { data } = await axios.get(`${process.env.BACKEND_URL}/me`);
+    const { data } = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/me`);
 
     dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
   } catch (error) {
@@ -92,7 +92,7 @@ export const loadUser = () => async (dispatch) => {
 // Logout User
 export const logout = () => async (dispatch) => {
   try {
-    await axios.get(`${process.env.BACKEND_URL}/logout`);
+    await axios.get(`${process.env.REACT_APP_BACKEND_URL}/logout`);
 
     dispatch({ type: LOGOUT_SUCCESS });
   } catch (error) {
@@ -107,7 +107,7 @@ export const updateProfile = (userData) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "multipart/form-data" } };
 
-    const { data } = await axios.put(`${process.env.BACKEND_URL}/me/update`, userData, config);
+    const { data } = await axios.put(`${process.env.REACT_APP_BACKEND_URL}/me/update`, userData, config);
 
     dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.success });
   } catch (error) {
@@ -126,7 +126,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.put(
-      `${process.env.BACKEND_URL}/password/update`,
+      `${process.env.REACT_APP_BACKEND_URL}/password/update`,
       passwords,
       config
     );
@@ -147,7 +147,7 @@ export const forgotPassword = (email) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "application/json" } };
 
-    const { data } = await axios.post(`${process.env.BACKEND_URL}/password/forgot`, email, config);
+    const { data } = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/password/forgot`, email, config);
 
     dispatch({ type: FORGOT_PASSWORD_SUCCESS, payload: data.message });
   } catch (error) {
@@ -166,7 +166,7 @@ export const resetPassword = (token, passwords) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.put(
-      `${process.env.BACKEND_URL}/password/reset/${token}`,
+      `${process.env.REACT_APP_BACKEND_URL}/password/reset/${token}`,
       passwords,
       config
     );
@@ -184,7 +184,7 @@ export const resetPassword = (token, passwords) => async (dispatch) => {
 export const getAllUsers = () => async (dispatch) => {
   try {
     dispatch({ type: ALL_USERS_REQUEST });
-    const { data } = await axios.get(`${process.env.BACKEND_URL}/admin/users`);
+    const { data } = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/admin/users`);
 
     dispatch({ type: ALL_USERS_SUCCESS, payload: data.users });
   } catch (error) {
@@ -196,7 +196,7 @@ export const getAllUsers = () => async (dispatch) => {
 export const getUserDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: USER_DETAILS_REQUEST });
-    const { data } = await axios.get(`${process.env.BACKEND_URL}/admin/user/${id}`);
+    const { data } = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/admin/user/${id}`);
 
     dispatch({ type: USER_DETAILS_SUCCESS, payload: data.user });
   } catch (error) {
@@ -212,7 +212,7 @@ export const updateUser = (id, userData) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.put(
-      `${process.env.BACKEND_URL}/admin/user/${id}`,
+      `${process.env.REACT_APP_BACKEND_URL}/admin/user/${id}`,
       userData,
       config
     );
@@ -231,7 +231,7 @@ export const deleteUser = (id) => async (dispatch) => {
   try {
     dispatch({ type: DELETE_USER_REQUEST });
 
-    const { data } = await axios.delete(`${process.env.BACKEND_URL}/admin/user/${id}`);
+    const { data } = await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/admin/user/${id}`);
 
     dispatch({ type: DELETE_USER_SUCCESS, payload: data });
   } catch (error) {
